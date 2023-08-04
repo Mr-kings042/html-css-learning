d12af91ffe6d4c1928098e24a9cd2ccf4af9c4bb
Okoh Kingsley <124823851+Mr-kings042@users.noreply.github.com>
2023-08-04 23:44:24 +0100

    Add files via upload

---
+++ HTML, CSS LEARNING/js/index.js
@@ -0,0 +1,48 @@
+// // document.getElementById
+// // console.log (101 > 100);
+// // console.log (10 > 100);
+// // console.log (200 !== 100)
+// // let weather = function(temperature){
+// //     console.log("The temperature outside is", temperature, "degrees farenheight.");
+// //  if(temperature <= 65) {
+// //     console.log ('temperature');
+// //   }
+// // }
+// //   weather (60);
+// //   let sellStock = function(stockPrice, sellPrice) {
+// //     //write your code here.
+// //     if(stockPrice >= sellPrice) {
+// //       console.log ("true");
+// //     }
+// //     else {
+// //       console.log ("false");
+// //     }
+// //   }
+// //   sellStock(500,500);
+// //   sellStock(70,500);
+// //   sellStock(500,500);
+// //   sellStock(500,500);
+
+//   let num = 0;
+
+//   while (num <= 10){
+//     if (num !== 10) {
+//       console.log ("the number is", num);
+//     }
+//     else {
+//       console.log ("number", num);
+//     }
+//   num++;
+//   }
+//   var studentString = students.toString();
+// console.log(studentString);
+
+let date = new Date();
+date = date.toLocaleString();
+console.log (date);
+if (false != true) {
+    console.log("That makes sense.");
+    if (0 < 2) {
+    console.log("No surprise there.");
+    }
+    }
